05a5401f5b9ba4885b2c90e6be6fb39449c1f741
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2023-09-03 16:36:27 +0200

    update gitignore

---
+++ Lesson1/Lesson1/dist/models/esm2022/public-api.mjs
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of models
+ */
+export * from './lib/models.service';
+export * from './lib/models.component';
+export * from './lib/models.module';
+export * from './lib/course';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3Byb2plY3RzL21vZGVscy9zcmMvcHVibGljLWFwaS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7R0FFRztBQUVILGNBQWMsc0JBQXNCLENBQUM7QUFDckMsY0FBYyx3QkFBd0IsQ0FBQztBQUN2QyxjQUFjLHFCQUFxQixDQUFDO0FBRXBDLGNBQWMsY0FBYyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLypcbiAqIFB1YmxpYyBBUEkgU3VyZmFjZSBvZiBtb2RlbHNcbiAqL1xuXG5leHBvcnQgKiBmcm9tICcuL2xpYi9tb2RlbHMuc2VydmljZSc7XG5leHBvcnQgKiBmcm9tICcuL2xpYi9tb2RlbHMuY29tcG9uZW50JztcbmV4cG9ydCAqIGZyb20gJy4vbGliL21vZGVscy5tb2R1bGUnO1xuXG5leHBvcnQgKiBmcm9tICcuL2xpYi9jb3Vyc2UnO1xuIl19
---
+++ Lesson1/Lesson1/dist/models/public-api.d.ts
@@ -0,0 +1,4 @@
+export * from './lib/models.service';
+export * from './lib/models.component';
+export * from './lib/models.module';
+export * from './lib/course';
