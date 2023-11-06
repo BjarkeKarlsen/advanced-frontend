c8b2cd8415e0727b7a60d03ee5576d5dcd6aa9da
BjarkeKarlsen <bjarke.k22@gmail.com>
2023-11-06 16:09:36 +0100

    Chapter 7 WIP - VERCEL DONT WORK 6

--- Lesson10/nextjs-dashboard/app/lib/data.ts
+++ Lesson10/nextjs-dashboard/app/lib/data.ts
@@ -24,7 +24,7 @@ export async function fetchRevenue() {
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch complete after 3 seconds.');
-
+    console.error('Revenue:', data.rows);
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
